aed3ea815703c4492c27f24494ac1a8b46dadd36
MatveyVolkov <flexbookmatvey@icloud.com>
2021-07-01 23:26:22 +0200

    Completed tasks: 1, 2, 3, 4

--- lesson/Program.cs
+++ lesson/Program.cs
@@ -6,7 +6,7 @@ namespace Lesson
     {
         static void Main(string[] args)
         {
-
+            
             #region Программа «Анкета»
             Console.Write("Здравствуйте! Введите своё имя: ");
             string name = Console.ReadLine();
@@ -41,17 +41,45 @@ namespace Lesson
             Console.WriteLine("Перейти к следующей программе");
             Console.ReadKey();
             #endregion
-
-
             #region Рассчитать и вывести индекс массы
             Console.Write("Введите массу тела: ");
-            Int16 m = Int16.Parse(Console.ReadLine());
+            double m = double.Parse(Console.ReadLine());
 
             Console.Write("Введите рост тела(в метрах): ");
-            Int16 h = Int16.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
 
-            Console.WriteLine($"ИМТ = {m/(h * h)}");
+            Console.WriteLine($"ИМТ = {m / (h * h)}");
+
+            Console.WriteLine("Перейти к следующей программе");
+            Console.ReadKey();
             #endregion
+            #region Расстояние между точками
+            decimal dist = Convert.ToDecimal(getDistance(1.0, 1.0, 2.0, 2.0));
+
+            Console.WriteLine($"Расстояние между точками: {dist.ToString("F2")} ");
+            #endregion
+            #region Обмен значений int
+            // Задание без звездочки
+            int a = 1;
+            int b = 2;
+            int c = a;
+            a = b;
+            b = c;
+            Console.WriteLine($"a = {a} b = {b}");
+
+            // Задание со звездочкой
+            a = 1333;
+            b = 2123;
+            a = a ^ b;
+            b = a ^ b;
+            a = a ^ b;
+            Console.WriteLine($"a = {a} b = {b}");
+            #endregion
+        }
+
+        static double getDistance(double x1, double y1, double x2, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
         }
     }
 }
