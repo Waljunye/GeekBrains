ae698709b2ae3a07401967a391af5f6ac8534283
MatveyVolkov <flexbookmatvey@icloud.com>
2021-07-02 18:34:03 +0200

    HW Complited!

--- lesson/Program.cs
+++ lesson/Program.cs
@@ -2,6 +2,7 @@
 
 namespace Lesson
 {
+    
     class Program
     {
         static void Main(string[] args)
@@ -38,10 +39,10 @@ namespace Lesson
             Console.WriteLine("Форматированный вывод, используя $: ");
             Console.WriteLine($"Ваше имя: {name} \n Ваша фамилия: {surname} \n Ваш возраст: {age} \n Ваш рост: {height} \n Ваш вес: {weight}");
 
-            Console.WriteLine("Перейти к следующей программе");
-            Console.ReadKey();
+            Pause();
             #endregion
             #region Рассчитать и вывести индекс массы
+            // Просто введите ззначения, можно с плавающей точкой, можно без
             Console.Write("Введите массу тела: ");
             double m = double.Parse(Console.ReadLine());
 
@@ -50,13 +51,14 @@ namespace Lesson
 
             Console.WriteLine($"ИМТ = {m / (h * h)}");
 
-            Console.WriteLine("Перейти к следующей программе");
-            Console.ReadKey();
+            Pause();
             #endregion
             #region Расстояние между точками
+            // Тут по сути просто метод. Значения координат только в double
             decimal dist = Convert.ToDecimal(getDistance(1.0, 1.0, 2.0, 2.0));
 
             Console.WriteLine($"Расстояние между точками: {dist.ToString("F2")} ");
+            Pause();
             #endregion
             #region Обмен значений int
             // Задание без звездочки
@@ -74,12 +76,61 @@ namespace Lesson
             b = a ^ b;
             a = a ^ b;
             Console.WriteLine($"a = {a} b = {b}");
+
+            Pause();
+            #endregion
+            #region Текст-центрист
+            // Тут все просто. 1 свойство - параметр, 2 - x велечина
+            // Также есть реализация с y, этот параметр пишется сразу после x
+            Print("Матвей", Console.WindowWidth / 2);
+            Print("Волков", Console.WindowWidth / 2);
+            Print("Сургут", Console.WindowWidth / 2);
+            Pause();
             #endregion
         }
 
         static double getDistance(double x1, double y1, double x2, double y2)
         {
             return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+        }
+
+        static void Print(string msg, int x, int y)
+        {
+            for (int i = 0; i <= y; i++)
+            {
+                Console.Write("\n");
+            }
+            for (int i = 0; i <= x; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(msg);
+        }
+
+        static void Print(string msg, int x)
+        {
+            for (int i = 0; i <= x; i++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine(msg);
+        }
+
+        static void Print(string msg, bool newLine = true)
+        {
+
+            if (newLine)
+            {
+                Console.WriteLine(msg);
+            } else
+            {
+                Console.Write(msg);
+            }
+        }
+        static void Pause()
+        {
+            Console.WriteLine("Pause. Press any button to continue...");
+            Console.ReadKey();
         }
     }
 }
